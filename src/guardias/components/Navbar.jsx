662e43d0e5e943da9afe5af1a08daf4d8832f17a
JosepConejero <bermeu@gmail.com>
2024-06-12 18:38:49 +0200

    Copied Navbar.jsx from invited version and added next courses button

--- src/guardias/components/Navbar.jsx
+++ src/guardias/components/Navbar.jsx
@@ -28,7 +28,7 @@ export const Navbar = () => {
       }}
     >
       <Toolbar>
-        <IconButton color="inherit" edge="start" sx={{ mr: { xs: 0, md: 2 } }}>
+        <IconButton color="inherit" edge="start" sx={{ mr: 2 }}>
           <ImageListItem>
             <img src="/assets/logo-navbar.png" alt="logo de MPE"></img>
           </ImageListItem>
@@ -38,7 +38,7 @@ export const Navbar = () => {
           container
           direction={{ xs: "column", md: "row" }}
           justifyContent="space-between"
-          alignItems={{ xs: "flex-start", md: "center" }}
+          alignItems={{ xs: "flex-end", md: "center" }}
         >
           <Grid
             item
@@ -47,45 +47,29 @@ export const Navbar = () => {
             }}
           >
             <Typography variant="h6" component="div">
-              Calendario de guardias
-              <Typography
-                variant="span"
-                sx={{ display: { xs: "none", md: "inline" } }}
-              >
-                &nbsp;-&nbsp;{user.shortName}
-              </Typography>
+              Calendario de guardias - {user.shortName}
             </Typography>
           </Grid>
 
-          <Grid item>
-            <Grid container alignItems="center">
-              <Grid item sx={{ display: { md: "none" } }}>
-                <Typography variant="span" fontSize="20px">
-                  {user.shortName}
-                </Typography>
-              </Grid>
-              {/* <Grid item sx={{ mr: { xs: -1.5, md: -1 }, border: 1 }}> */}
-              <Grid item sx={{ mr: { md: -1 }, ml: { xs: 8 } }}>
-                <Link component={RouterLink} color="inherit" to="/guardias">
-                  <IconButton color="inherit">
-                    <PendingActionsIcon />
-                  </IconButton>
-                </Link>
-                <Link component={RouterLink} color="inherit" to="/guardias">
-                  <IconButton color="inherit">
-                    <CalendarMonthIcon />
-                  </IconButton>
-                </Link>
-                <Link component={RouterLink} color="inherit" to="/settings">
-                  <IconButton color="inherit">
-                    <SettingsIcon />
-                  </IconButton>
-                </Link>
-                <IconButton color="error" onClick={startLogout}>
-                  <LogoutOutlined />
-                </IconButton>
-              </Grid>
-            </Grid>
+          <Grid item sx={{ mr: { xs: -1.5, md: -1 } }}>
+            <Link component={RouterLink} color="inherit" to="/guardias">
+              <IconButton color="inherit">
+                <PendingActionsIcon />
+              </IconButton>
+            </Link>
+            <Link component={RouterLink} color="inherit" to="/guardias">
+              <IconButton color="inherit">
+                <CalendarMonthIcon />
+              </IconButton>
+            </Link>
+            <Link component={RouterLink} color="inherit" to="/settings">
+              <IconButton color="inherit">
+                <SettingsIcon />
+              </IconButton>
+            </Link>
+            <IconButton color="error" onClick={startLogout}>
+              <LogoutOutlined />
+            </IconButton>
           </Grid>
         </Grid>
       </Toolbar>
